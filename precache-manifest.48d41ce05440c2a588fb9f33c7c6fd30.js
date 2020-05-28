2f01c3e1d28ed7a1473ebe9155e58b142c7c086f
Aswad Ali <aswadgemini11@gmail.com>
2020-05-28 18:12:00 +0500

    Fixed resource links

---
+++ precache-manifest.48d41ce05440c2a588fb9f33c7c6fd30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c2233633f9830892f6356164af219a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b76416b9affe27726344",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "85b6c9adba92919a941b",
+    "url": "/static/css/main.496cbf87.chunk.css"
+  },
+  {
+    "revision": "b76416b9affe27726344",
+    "url": "/static/js/2.a91c909a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a91c909a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b6c9adba92919a941b",
+    "url": "/static/js/main.8eb4e231.chunk.js"
+  },
+  {
+    "revision": "caa60c7026981e60952d",
+    "url": "/static/js/runtime-main.92ef2979.js"
+  }
+]);
