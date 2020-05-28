16f66b19404bfabc28b7539133e9044f20185a38
Aswad Ali <aswadgemini11@gmail.com>
2020-05-28 18:35:52 +0500

    Fixed link issues

---
+++ precache-manifest.e3999d21db561cc51203d6453359e0c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2fccf6dc53d0059fa5742bddde92dc4",
+    "url": "/dfa-minimizer/index.html"
+  },
+  {
+    "revision": "afc43c006c14c0e7cfa6",
+    "url": "/dfa-minimizer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "14d4e2010a387de08e31",
+    "url": "/dfa-minimizer/static/css/main.496cbf87.chunk.css"
+  },
+  {
+    "revision": "afc43c006c14c0e7cfa6",
+    "url": "/dfa-minimizer/static/js/2.4eebf5c7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dfa-minimizer/static/js/2.4eebf5c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14d4e2010a387de08e31",
+    "url": "/dfa-minimizer/static/js/main.85dcc2f6.chunk.js"
+  },
+  {
+    "revision": "c58f0121ded3209ec5b2",
+    "url": "/dfa-minimizer/static/js/runtime-main.f4caf8ea.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.030d0d4d58776bb85ae8d182ded5b729.js"
+  "/dfa-minimizer/precache-manifest.e3999d21db561cc51203d6453359e0c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dfa-minimizer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
